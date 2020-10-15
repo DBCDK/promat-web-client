b82ccef812d5fccfad82aa7d6affd4637cc58541
Ditlev Tøjner (konsdito) <ditlev@lundogbendsen.dk>
2020-10-15 15:28:49 +0200

    Iteartion on DBC generic table

--- src/DBCDesign/components/Table.js
+++ src/DBCDesign/components/Table.js
@@ -1,36 +1,54 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import Table from 'react-bootstrap/Table'
-import PropTypes from 'prop-types';
-import DBCButton from './Button';
+import Form from 'react-bootstrap/Form'
+import {Row, Col} from 'react-bootstrap'
 
 export default class DBCTable extends React.Component {
     render() {
         return (
-            <Table responsive>
-            <thead>
-                <tr>
-                {this.props.header.map((el, index) => (
-                    <th key={index}>{el.label}</th>
-                ))}
-                </tr>
-            </thead>
-            <tbody>
+            <Row>
+                <Col sm={8}></Col>
+                
+                {
+                    this.props.searchable && (
+                        <Col sm={4}>
+                            <Form.Control type="text" placeholder="Filtrer på anmeldere" />
+                        </Col>
+                    )
+                }
+                
+                <Col sm={12}>
+                <Table responsive>
+                <thead>
+                    <tr>
+                    {this.props.header.map((el, index) => (
+                        <th key={index} className="font-bold">{el.label}</th>
+                    ))}
+                    </tr>
+                </thead>
+                <tbody>
 
                 {this.props.data.map((el,i) => {
                     return (
-                        <tr key={i} onClick={() => this.props.onClickRow(el)}>
+                        <tr key={i} {...this.props.rowCheckbox ? ({onClick:(e) => this.props.onClickRow(e)}) : {}}>
                             {this.props.header.reduce((sorted, key, index) => {
                                 sorted.push(el[key.name])
                                 return sorted
-                            },[]).map((e, index) => (
+                            },[]).map((e, index) => index === 0 && this.props.rowCheckbox ? (
+                                <th key={index}><Form.Check type={this.props.rowCheckboxAs || "checkbox"} onChange={() => this.props.onClickRow(e)} label={e} /></th>
+                            ) : (
                                 <th key={index}>{e}</th>
                             ))}
                         </tr>
                     )
                 })}
 
-            </tbody>
-            </Table>
+                </tbody>
+                </Table>
+                </Col>
+            </Row>
         )
     }
 }
