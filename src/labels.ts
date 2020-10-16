f97d5b2401a23a68de565e46960662620b5501cd
Ditlev Tøjner (konsdito) <ditlev@lundogbendsen.dk>
2020-10-16 15:41:34 +0200

    added labels (mediatypes)

---
+++ src/labels.ts
@@ -0,0 +1,9 @@
+import { MediaType } from "./@promat/shared/types/general";
+
+export const mediaTypeLabels = {
+    [MediaType.BOOK]: "Bog",
+    [MediaType.EBOOK]: "Ebog",
+    [MediaType.MOVIE]: "Film",
+    [MediaType.MULTIMEDIA]: "Multimedier",
+    [MediaType.MUSIC]: "Musik",
+}
