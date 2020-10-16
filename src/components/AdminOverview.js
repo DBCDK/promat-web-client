0c6d3c96b5a7c6fa0c83ceb03e603b829fd77df9
Ditlev Tøjner (konsdito) <ditlev@lundogbendsen.dk>
2020-10-16 15:38:47 +0200

    iteartion on adminoverview

--- src/components/AdminOverview.js
+++ src/components/AdminOverview.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DBCTable from '../DBCDesign/components/Table'
 import DBCButton, {DBCButtonGroup} from '../DBCDesign/components/Button'
 import {Container, Row, Col} from 'react-bootstrap'
 import {Link} from "react-router-dom";
+import PromatProvider from './PromatProvider';
+
+import {CaseStatus} from '../@promat/shared/types/case'
+import { MediaType } from "../@promat/shared/types/general";
+import {mediaTypeLabels} from '../labels'
 
 const header = [
     {label:"Titel",name:"title"},
@@ -14,68 +19,67 @@ const header = [
     {label:"Ugekode",name:"weekcode"},
     {label:"Status",name:"status"},
   ]
+
+function StatusBadge(status) {
   
-  const data = [
-    {
-      title:"Doktor kold og eventyrspejlet",
-      details:"Hestevej 22",
-      assignTo:"Julie Arndrup",
-      faustNo:"54330022",
-      assignDate:"03-08-2020",
-      deadline:"17-08-2020",
-      weekcode:"LEK202040",
-      status:"Overskredet frist",
-    },
-    {
-      title:"Doktor kold og eventyrspejlet",
-      details:"Hestevej 22",
-      assignTo:"Julie Arndrup",
-      faustNo:"54330022",
-      assignDate:"03-08-2020",
-      deadline:"17-08-2020",
-      weekcode:"LEK202040",
-      status:"Overskredet frist",
-    },
-    {
-      title:"Doktor kold og eventyrspejlet",
-      details:"Hestevej 22",
-      assignTo:"Julie Arndrup",
-      faustNo:"54330022",
-      assignDate:"03-08-2020",
-      deadline:"17-08-2020",
-      weekcode:"LEK202040",
-      status:"Overskredet frist",
-    },
-    {
-      title:"Doktor kold og eventyrspejlet",
-      details:"Hestevej 22",
-      assignTo:"Julie Arndrup",
-      faustNo:"54330022",
-      assignDate:"03-08-2020",
-      deadline:"17-08-2020",
-      weekcode:"LEK202040",
-      status:"Overskredet frist",
-    },
-  ]
+  const TagWrap = (label, style) => (<div className="table-status-tag" style={style}>{label}</div>)
+  switch (status) {
+    case "PUBLISHED":
+      return TagWrap(status, {backgroundColor:"green",color:"white"})
+    case "REQUESTED":
+    case "AWAITING_APPROVAL":
+      return TagWrap(status, {backgroundColor:"#FFC107",color:"white"})
+    default:
+      return TagWrap(status, {backgroundColor:"red",color:"white"})
+  }
+
+  const tagStyle = {backgroundColor:"green",color:"white"}
+  console.log(status)
+  return TagWrap(status)
+}
 
 function OverviewTable(props) {
+
+    const [filter, setFilter] = useState(false)
+    const filterType = (child) => child.mediaType === filter
+    const data = props.children ? filter ? props.children.filter(filterType) : props.children : []
 
     return (
       <div className="promat-container">
       <Row noGutters={true}>
         <Col sm={8} className="promat-container-header">
-          <h3>{props.label}</h3>
+        <h3>{props.label} – {data.length}</h3>
         </Col>
         <Col sm={4} className="align-right">
         <DBCButtonGroup>
-          <DBCButton>Bøger</DBCButton>
-          <DBCButton>Film</DBCButton>
-          <DBCButton>Multimedier</DBCButton>
+          {
+            [ // add mediatypes as filters here
+              MediaType.BOOK,
+              MediaType.MOVIE,
+              MediaType.MULTIMEDIA
+            ].map((mediaType,i) => {
+              if (filter && filter === mediaType) {
+                return (
+                  <DBCButton key={i} onClick={() => setFilter(false)}>{mediaTypeLabels[mediaType]}</DBCButton>
+                )
+              }
+              else {
+                return (
+                  <DBCButton variant="secondary" key={i} onClick={() => setFilter(mediaType)}>{mediaTypeLabels[mediaType]}</DBCButton>
+                  )
+              }
+            })
+          }
+          
         </DBCButtonGroup>
         </Col>
         <Col className="margin-top-20">
         
-        <DBCTable header={header} data={data} />
+        <DBCTable header={header} data={data.map((c) => ({
+          ...c,
+          faustNo:c.id,
+          status: StatusBadge(c.status)
+        }))} />
         
         </Col>
       </Row>
@@ -83,20 +87,37 @@ function OverviewTable(props) {
     )
 }
 
+function CasesOverview(props) {
+  const sortedCases = props.cases.reduce((sorted, theCase) => {
+    if (sorted.has(theCase.status)) {
+      sorted.set(theCase.status, [...sorted.get(theCase.status), theCase])
+    }
+    else {
+      sorted.set(theCase.status, [theCase])
+    }
+    return sorted
+  }, new Map())
+
+  return Object.keys(CaseStatus).map((status) => (
+  <OverviewTable label={status}>{sortedCases.get(status)}</OverviewTable>
+  ))
+}
+
 export default function AdminOverview(props) {
   return (
       <Container>
         <Row>
+          
           <Col sm={8}>
             <h1>Forside</h1>
           </Col>
+          
           <Col sm={4} className="align-right"><Link to="/create-case"><DBCButton>Opret sag</DBCButton></Link></Col>
+          
           <Col>
-          
-          {["Afventer godkendelse","Afviste anmeldelser","Afventer"].map((key) => (
-              <OverviewTable label={key}></OverviewTable>
-          ))}
-          
+            <PromatProvider resource="cases" loading={() => (<div>Skaber overblik...</div>)}>
+              <CasesOverview />
+            </PromatProvider>
           </Col>
         </Row>
       </Container>
